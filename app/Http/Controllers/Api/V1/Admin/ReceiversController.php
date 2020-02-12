58f16da5dbbb566c35fef5878c0e75af36d50789
Leiviton Carlos <leiviton.silva@drsgroup.com.br>
2020-02-12 12:28:19 +0000

    ReceiversController.php edited online with Bitbucket

--- app/Http/Controllers/Api/V1/Admin/ReceiversController.php
+++ app/Http/Controllers/Api/V1/Admin/ReceiversController.php
@@ -55,7 +55,7 @@ class ReceiversController extends Controller
      */
     public function edit($id)
     {
-        return $this->service->getId($id);
+        return $this->service->getId($id,false);
     }
 
     /**
